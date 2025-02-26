69e158b1bb65f05e3568dbe8aa5bdbbb19f4adc6
Wenchao Wang <wang1021@algonquinlive.com>
2025-02-26 10:47:21 -0500

    lab6: changed

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar( backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title)),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               //TextField for input;
               TextField(controller: _controllerItem,
